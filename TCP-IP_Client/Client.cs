2b1ffbcaacd9b13e56b3f30c31532d7c573943f7
Amin Bondi <2212@holbertonschool.com>
2023-01-31 19:39:44 +0100

    Client side

---
+++ TCP-IP_Client/Client.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+string hostName = Dns.GetHostName();
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(hostName);
+IPAddress ipAdress = ipHostInfo.AddressList[0];
+IPEndPoint ipEndPoint = new(ipAdress, 11_000);
+
+using Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+await client.ConnectAsync(ipEndPoint);
+while (true)
+{
+    sendMessage();
+    receiveMessage();
+}
+
+async void sendMessage()
+{
+    var message = Console.ReadLine();
+    try
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        _ = await client.SendAsync(messageBytes, SocketFlags.None);
+        Console.WriteLine($"Me: {message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+
+async void receiveMessage()
+{
+    var buffer = new byte[1024];
+    try
+    {
+        var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+        var response = Encoding.UTF8.GetString(buffer, 0, received);
+        Console.WriteLine($"Server: {response}");
+    }
+    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+}
